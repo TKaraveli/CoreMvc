080e3377dffa974a73742fb71f444189da50dc45
TKaraveli <tayfunkaraveli@hotmail.com>
2020-03-02 10:52:50 +0300

    Add project files.

---
+++ CourseApp2/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseApp2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            CourseApp2.Models.Request model = new Request();
+
+            model.Name = "Tayfun Karaveli";
+            model.Email = "tayfunkaraveli@hotmail.com";
+            model.Phone = "0123456789";
+            model.Message = "Kursa katılmak istiyorum.";
+
+            return View(model);
+        }
+    }
+}
