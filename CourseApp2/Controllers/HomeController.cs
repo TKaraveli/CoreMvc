8db2cb3e9d0ac3dfecd99b5d0e53288ee278a7f3
TKaraveli <tayfunkaraveli@hotmail.com>
2020-03-05 16:59:12 +0300

    .

--- CourseApp2/Controllers/HomeController.cs
+++ CourseApp2/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CourseApp2.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace CourseApp2.Controllers
 {
@@ -11,14 +12,27 @@ namespace CourseApp2.Controllers
     {
         public IActionResult Index()
         {
-            CourseApp2.Models.Request model = new Request();
+            //CourseApp2.Models.Request model = new Request();
 
-            model.Name = "Tayfun Karaveli";
-            model.Email = "tayfunkaraveli@hotmail.com";
-            model.Phone = "0123456789";
-            model.Message = "Kursa katılmak istiyorum.";
+            //model.Name = "Tayfun Karaveli";
+            //model.Email = "tayfunkaraveli@hotmail.com";
+            //model.Phone = "0123456789";
+            //model.Message = "Kursa katılmak istiyorum.";
 
-            return View(model);
+            return View();
         }
+
+        [HttpGet]
+        public IActionResult AddRequest()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddRequest(Request model)
+        {
+            return View();
+        }
+
     }
 }
---
+++ CourseApp2/Migrations/20200303063045_CreateDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseApp2.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Message = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Requests");
+        }
+    }
+}
